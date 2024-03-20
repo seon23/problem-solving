1d6333fe67034080ffaffd017a0eb332a511fcde
Seon <120017668+seon23@users.noreply.github.com>
2024-03-21 08:32:39 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,7 @@
+const solution = price => 
+    Math.floor(
+        price >= 500000 ? price * 0.8
+      : price >= 300000 ? price * 0.9
+      : price >= 100000 ? price * 0.95
+      : price
+    )
