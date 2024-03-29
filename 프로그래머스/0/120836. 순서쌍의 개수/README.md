637aa1f8a5af01e473d27dae5e3227c360f3a985
Seon <120017668+seon23@users.noreply.github.com>
2024-03-29 11:15:04 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/README.md
+++ 프로그래머스/0/120836. 순서쌍의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 순서쌍의 개수 - 120836 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120836?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120836) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 0.27 ms
+메모리: 33.5 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 29일 11:00:41
+2024년 03월 29일 11:15:03
 
 ### 문제 설명
 
--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,13 +1,9 @@
 
 const solution = (n) => {
-  const pairs = { 1: n };
-
-  for (let divisor = 2; !pairs[divisor] && divisor <= n / 2; divisor += 1) {
-    if (n % divisor !== 0) continue;
-    pairs[n / divisor] = divisor;
-  }
-
-  return Math.sqrt(n) % 1 === 0
-    ? Object.keys(pairs).length * 2 - 1
-    : Object.keys(pairs).length * 2;
+    let ret = 0; 
+    for (let i = 1; i < Math.sqrt(n); i += 1) {
+        if (n % i === 0) ret += 2;
+    }
+    
+    return Math.sqrt(n) % 1 === 0 ? ret += 1: ret;
 };
