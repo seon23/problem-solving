681e89e4d0c57797fee38c364605b2132f912562
Seon <pqpqpq460@gmail.com>
2024-04-07 19:29:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,5 +1,6 @@
 const solution = (num, cnt = 0) => {
     if (cnt > 500) return -1;
     if (num === 1) return cnt;
-    return (num % 2 === 0) ? solution(num / 2, cnt + 1) : solution(3 * num + 1, cnt + 1);
+    
+    return solution((num % 2 === 0 ? num / 2 : num * 3 + 1), cnt + 1);
 }
