1525a415ead98a15deaa6fe452c6114f85992281
Seon <pqpqpq460@gmail.com>
2024-04-11 00:24:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,4 +1,4 @@
 const solution = (s) => {
     const half = s.length / 2;
-    return half % 1 === 0 ? s.substring(half - 1, half + 1) : s[parseInt(half)];
+    return s.substr(Math.round(half) - 1, half % 1 === 0 ? 2 : 1);
 }
