d667d914fe9ef2df7bb4b85ec95aef668e4f5023
Seon <pqpqpq460@gmail.com>
2024-04-17 00:22:10 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.23 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,7 +1,8 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-    const [n, m] = data.split(" ").map((v) => Number(v));
-    for (let i = 0; i < m; i += 1) {
-        console.log('*'.repeat(n));
+    const n = data.split(" ");
+    const [a, b] = [Number(n[0]), Number(n[1])];
+    for (let i = 0; i < b; i += 1) {
+        console.log('*'.repeat(a));
     }
 });
