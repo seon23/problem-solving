0b355f69360d65b78aaa4505302d9f65bbcfa409
Seon <pqpqpq460@gmail.com>
2024-04-25 04:34:35 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,10 +1,1 @@
-function solution(hp) {
-    let [left, num] = [hp, 0];
-    
-    for (const power of [5, 3, 1]) {
-        if (left % power === 0) return num + (left / power);
-        [left, num] = [left % power, num + parseInt(left / power)];
-    }
-    
-    return num;
-}
+const solution = (hp) => Math.trunc(hp / 5) + Math.trunc((hp % 5) / 3) + (hp % 5 % 3);
